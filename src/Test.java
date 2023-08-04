de828c95b3e4522370ae8929e2a5a366d31b9473
linda <linda@163.com>
2023-08-04 18:23:29 +0800

    提交所有

--- src/Test.java
+++ src/Test.java
@@ -8,5 +8,9 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("bcccc");
+        System.out.println("wwww");
+        System.out.println("eeeee");
+        System.out.println("eeeeee");
+        System.out.println("555");
     }
 }
