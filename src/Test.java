9b5ad00456b48af3f441931626dce29b30997a05
linda <linda@163.com>
2023-08-04 18:29:12 +0800

    Revert "提交所有"
    This reverts commit de828c95b3e4522370ae8929e2a5a366d31b9473.

--- src/Test.java
+++ src/Test.java
@@ -8,9 +8,5 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("bcccc");
-        System.out.println("wwww");
-        System.out.println("eeeee");
-        System.out.println("eeeeee");
-        System.out.println("555");
     }
 }
